7746d6414e8715438d7d21fadea4a8d7e4ae69e6
Surya Tejess Karri <suryatejessk@gmail.com>
2024-10-24 16:46:33 +0530

    :heavy_plus_sign: feat: Adding Main Menu scene for the game

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import k from "./kaplayCtx.js";
+import mainMenu from "./scenes/mainMenu.js";
 
 // Loading assets
 k.loadSprite("chemical-bg", "graphics/chemical-bg.png"); // you dont need to mention "public" when the assets are in the "public" folder
@@ -32,3 +33,9 @@ k.loadSound("hyper-ring", "sounds/HyperRing.wav");
 k.loadSound("jump", "sounds/Jump.wav");
 k.loadSound("ring", "sounds/Ring.wav");
 k.loadSound("city", "sounds/city.mp3");
+
+k.scene("main-menu", mainMenu);
+k.scene("game", () => {});
+k.scene("gameover", () => {});
+
+k.go("main-menu");
