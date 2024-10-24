ccd2706048772563c1e8ff1faa01ad05cbfdfacc
Surya Tejess Karri <suryatejessk@gmail.com>
2024-10-24 15:14:38 +0530

    :heavy_plus_sign: feat:Loading Assets

--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
 import k from "./kaplayCtx.js";
+
+// Loading assets
+k.loadSprite("chemical-bg", "graphics/chemical-bg.png"); // you dont need to mention "public" when the assets are in the "public" folder
+k.loadSprite("platforms", "graphics/platforms.png");
+k.loadSprite("sonic", "graphics/sonic.png", {
+  sliceX: 8,
+  sliceY: 2,
+  anims: {
+    run: { from: 0, to: 7, loop: true, speed: 30 },
+    jump: { from: 8, to: 15, loop: true, speed: 100 },
+  },
+});
+k.loadSprite("ring", "graphics/ring.png", {
+  sliceX: 16,
+  sliceY: 1,
+  spin: { from: 0, to: 15, loop: true, speed: 30 },
+});
+k.loadSprite("motobug", "graphics/motobug.png", {
+  sliceX: 5,
+  sliceY: 1,
+  spin: { from: 0, to: 4, loop: true, speed: 8 },
+});
+
+// Loading Fonts
+k.loadFont("mania", "fonts/mania.ttf");
+
+// Loading Sounds
+k.loadSound("destroy", "sounds/Destroy.wav");
+k.loadSound("hurt", "sounds/Hurt.wav");
+k.loadSound("hyper-ring", "sounds/HyperRing.wav");
+k.loadSound("jump", "sounds/Jump.wav");
+k.loadSound("ring", "sounds/Ring.wav");
+k.loadSound("city", "sounds/city.mp3");
